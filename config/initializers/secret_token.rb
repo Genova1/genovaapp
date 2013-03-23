2be49cb66fc48758350a9c40fc2daa7b94a175e9
James Genova <jgenova01@gmail.com>
2013-03-23 14:51:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genovaapp::Application.config.secret_token = '7bab7b0839a1def5d12b734d4a71f5983499c63b92b2a09a7fddca4e36e9eeb27eadbe1ec4cf4c7e487ad5b9b2401b33fcf01d4c11540361a20cdc280c2654b0'
